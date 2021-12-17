003a7826b65c0630884aaa346c7892972176a488
Jothis <jothissusanthomas4@gmail.com>
2021-12-17 15:04:37 +0530

    added the files

---
+++ hello.java
@@ -0,0 +1,18 @@
+ /*Volume in drive C is OS
+ Volume Serial Number is 3CCE-7A3A
+
+ Directory of C:\Users\Jothis Thomas\Desktop\Jenkins demo
+
+17-12-2021  14:49    <DIR>          .
+17-12-2021  14:49    <DIR>          ..
+17-12-2021  14:49                 0 hello.java
+17-12-2021  14:48               351 hello.txt
+               2 File(s)            351 bytes
+               2 Dir(s)  133,802,405,888 bytes free
+*/
+public class hello {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Everyone!");
+    }
+}
